85cc191e0e8aa39ed2cec411cde48eee28ee8532
Nusantara Builder <builder@nusantara.dev>
2025-07-15 23:27:12 +0700

    feat: Initial website deployment

---
+++ _next/static/ZO0ZxDC9wJRK6zX6UuUo_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c641a17854f83280.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
